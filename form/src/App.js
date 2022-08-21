65d860602619f6b661f58791443bfeb414288034
manzeelanepali <nepalimanzeela@gmail.com>
2022-08-21 23:01:30 +0545

    passing props

--- form/src/App.js
+++ form/src/App.js
@@ -2,21 +2,28 @@
 
 
 import { useState } from 'react'
-import Note from './components/Note'
+import Note from './Components/Note'
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
+  console.log(notes)
 
-  return (
-    <div>
+
+  return ( 
+    <div>  
       <h1>Notes</h1>
       <ul>
         {notes.map(note => 
           <Note key={note.id} note={note} />
         )}
       </ul>
+      
+    {/* <form>
+        <button>click me </button>
+        <input />
+        </form> */}
     </div>
   )
 }
 
-export default App 
+export default App  
