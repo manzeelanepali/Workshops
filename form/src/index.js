79d769abdd2cc55e888cb239edc96be1d6760b53
manzeelanepali <nepalimanzeela@gmail.com>
2022-08-29 14:26:56 +0545

    Understanding promises and async nature of axios

--- form/src/index.js
+++ form/src/index.js
@@ -3,31 +3,46 @@ import  ReactDOM  from "react-dom/client";
 import App from "./App";  
 import axios from "axios";
 
-// const notes = [
-//     {
-//       id: 1,
-//       content: 'HTML is easy',
-//       date: '2019-05-30T17:30:31.098Z',     
-//       important: true
-//     },
-//     {
-//       id: 2,
-//       content: 'Browser can execute only JavaScript',
-//       date: '2019-05-30T18:39:34.091Z',
-//       important: false
-//     },
-//     {
-//       id: 3,
-//       content: 'GET and POST are the most important methods of HTTP protocol',
-//       date: '2019-05-30T19:20:14.298Z',
-//       important: true
-//     },
-// ]; 
-axios.get('http://localhost:3001/notes').then((response)=>{
-  console.dir(response.data);
+const notes = [
+    {
+      id: 1,
+      content: 'HTML is easy',
+      date: '2019-05-30T17:30:31.098Z',     
+      important: true
+    },
+    {
+      id: 2,
+      content: 'Browser can execute only JavaScript',
+      date: '2019-05-30T18:39:34.091Z',
+      important: false
+    },
+    {
+      id: 3,
+      content: 'GET and POST are the most important methods of HTTP protocol',
+      date: '2019-05-30T19:20:14.298Z',
+      important: true
+    },
+]; 
+let axiosNotes = null;
+// axios.get('http://localhost:3001/notes').then((response)=>{
+//   notes = response.data
+// });
+ axios.
+ get('http://localhost:3001/notes')
+ .then((result)=>{
+  console.log(result)
+ 
+   ReactDOM.createRoot(document.getElementById("root")).render(
+ <App notes={result.data}/>
+ );
+   })
 
-  ReactDOM.createRoot(document.getElementById("root")).render(
-<App notes={response.data}/>
-);
+.catch((error)=> console.log(error));
 
-});
+ 
+
+// axiosReturn.then((res)=>{console.log("result from successful axios call",res)})
+// console.log("returned from axios is",axiosReturn.promiseState)
+// console.log("after calling axios.get",notes);
+
+
