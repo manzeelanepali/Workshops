67233dba51fced0249a4d3b3b67fdc52501c6cc4
manzeelanepali <nepalimanzeela@gmail.com>
2022-09-06 16:57:58 +0545

    when clicked the button console display buttonClik

--- form/src/Components/Note.js
+++ form/src/Components/Note.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const Note = ({note}) => {
+  const  clickFunc=()=>{
+    console.log("Button is clicked ")
+
+  }
   
   return (
-    <li>{note.content}({note.important.toString()})<button>Change  importance </button></li>
+    <li>{note.content}({note.important.toString()})<button onClick={clickFunc}>Change  importance </button></li>
   )
 }
 
