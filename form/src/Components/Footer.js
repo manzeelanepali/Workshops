6c5f7774550bf3c6d5d3dabb2ad83b01c2925135
manzeelanepali <nepalimanzeela@gmail.com>
2022-09-16 11:04:12 +0545

    adding color in the footer

--- form/src/Components/Footer.js
+++ form/src/Components/Footer.js
@@ -1,16 +1,18 @@
 const Footer = () => {
-    const footerStyle={
-        color: "green",
-        fontStyle: "italic",
-        fontSize : 16,
-
-    } ;
-   
-    return (
-      <div  styles ={footerStyle}>
-        <br />
-        <em>Note app, TEJ Center ,2022</em>
-      </div>
-    );
+  let footerStyle = {
+    color: "green",
+    fontStyle: "italic",
+    fontSize: 16,
   };
-  export default Footer;
+  // if (true) {
+  //   footerStyle = { ...footerStyle, color: red };
+  // }
+
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, TEJ Center ,2022</em>
+    </div>
+  );
+};
+export default Footer;
