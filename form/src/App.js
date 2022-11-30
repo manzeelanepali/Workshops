e3775055feff9141fc45a29c8fd4a43501505cbd
manzeelanepali <nepalimanzeela@gmail.com>
2022-11-30 15:12:42 +0545

    feat : made the loginform toggable by wrapping the login function inside of toggable compo

--- form/src/App.js
+++ form/src/App.js
@@ -6,6 +6,7 @@ import notesService from "./services/notes";
 import Notifications from "./Components/Notifications";
 import loginService from "./services/login";
 import Togglable from "./Components/Togglable";
+import LoginForm from "./Components/LoginForm";
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note");
@@ -86,27 +87,15 @@ const App = () => {
   };
 
   const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-        <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password
-        <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>
+    <Togglable buttonLabel="login">
+      <LoginForm
+        username={username}
+        password={password}
+        handleUsernameChange={({ target }) => setUsername(target.value)}
+        handlePasswordChange={({ target }) => setPassword(target.value)}
+        handleSubmit={handleLogin}
+      />
+    </Togglable>
   );
 
   const noteForm = () => (
@@ -117,7 +106,80 @@ const App = () => {
     </form>
   );
 
-  return <Togglable buttonLabel="show me">I am manjila </Togglable>;
+  return (
+    <div>
+      <h1>Notes details</h1>
+      <Notifications message={message} />
+
+      {/* {user === null ? loginForm() : noteForm()} */}
+      {user === null ? (
+        loginForm()
+      ) : (
+        <div>
+          <p>{user.name} logged-in</p>
+          {noteForm()}
+        </div>
+      )}
+
+      <button onClick={() => setShowALL(!showAll)}>
+        show {showAll ? "important" : "all"}
+      </button>
+
+      <ul>
+        {/* {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )} */}
+
+        {notesToShow.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            const
+            toggleImportance={() => {
+              console.log(
+                `Button is clicked bu function passsed from App for id ${note.id}`
+              );
+              // points for updating the value in the backend only
+              // 1. Make new object from current note with toggled important field
+              const updatedNotes = { ...note, important: !note.important };
+              // axios
+              notesService
+                .update(note.id, updatedNotes)
+                .then((response) => {
+                  setNotes(notes.map((x) => (x.id !== note.id ? x : response)));
+                  // setNotes(notes.concat(updatedNotes));
+                  // setNewNote("");
+                })
+                .catch((error) => {
+                  // console.log("caught the error ");
+                  setErrorMessage("This note doesnot exist anymore");
+                  setErrorMessage("This note doesnot exist anymore");
+                  setTimeout(() => setErrorMessage(null), 2000);
+                });
+              // }
+              //   // 2. update backend server with the updated object
+              //   .put(`http://localhost:3001/notes/${note.id}`, updatedNotes)
+              //   .then((response) => {
+              //     console.log(response.data);
+              //     // 3. put  for updating the frontend state with the updated note
+              //     setNotes(
+              //       notes.map((x) => (x.id !== note.id ? x : response.data))
+              //     );
+              //     // setNotes(notes.concat(newObject));
+              //     // setNewNote("");
+              //   });
+            }}
+          />
+        ))}
+      </ul>
+
+      {/* <form onSubmit={addNotes}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save </button>
+      </form> */}
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
