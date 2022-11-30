a0f232332ecd70bc04ee1ae70c426621679b2bc4
manzeelanepali <nepalimanzeela@gmail.com>
2022-11-30 14:22:34 +0545

    feat : setting the  visible states value true in the component toggleable .js to see the changes

--- form/src/Components/Togglable.js
+++ form/src/Components/Togglable.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const Togglable = (props) => {
-  const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState(true);
 
   const hideWhenVisible = { display: visible ? "none" : "" };
   const showWhenVisible = { display: visible ? "" : "none" };
