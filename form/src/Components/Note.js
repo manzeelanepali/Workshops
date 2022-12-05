0808ef1d02ccb549fa17dd53d79bf374b9a5aa3d
manzeelanepali <nepalimanzeela@gmail.com>
2022-12-05 16:54:32 +0545

    feat : teat cases passed

--- form/src/Components/Note.js
+++ form/src/Components/Note.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Note = ({ note, toggleImportance }) => {
   return (
-    <li>
-      {note.content}({note.important.toString()})
-      <button onClick={toggleImportance}>Change importance </button>
-    </li>
+    <>
+      <li className="note">{note.content}</li>
+      <button onClick={toggleImportance}>Change important</button>
+    </>
   );
 };
 
