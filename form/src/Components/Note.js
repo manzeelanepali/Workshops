f54e37dc5c34d44d9f8be62690294dbacd71cfb1
manzeelanepali <nepalimanzeela@gmail.com>
2022-12-06 10:12:27 +0545

    feat : clicking buttons in test

--- form/src/Components/Note.js
+++ form/src/Components/Note.js
@@ -4,7 +4,7 @@ const Note = ({ note, toggleImportance }) => {
   return (
     <>
       <li className="note">{note.content}</li>
-      <button onClick={toggleImportance}>Change important</button>
+      <button onClick={toggleImportance}>Change importance</button>
     </>
   );
 };
--- form/src/Components/Note.test.js
+++ form/src/Components/Note.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Note from "./Note";
 
 test("renders content", () => {
@@ -9,7 +10,11 @@ test("renders content", () => {
     important: true,
   };
 
-  const { container } = render(<Note note={note} />);
+  const mockHandler = jest.fn();
+
+  const { container } = render(
+    <Note note={note} toggleImportance={mockHandler} />
+  );
 
   const div = container.querySelector(".note");
   expect(div).toHaveTextContent(
@@ -22,4 +27,21 @@ test("renders content", () => {
 
   screen.debug(element);
   expect(element).toBeDefined();
+});
+
+test("clicking the button calls event handler once", async () => {
+  const note = {
+    content: "Component testing is done with react-testing-library",
+    important: true,
+  };
+
+  const mockHandler = jest.fn();
+
+  render(<Note note={note} toggleImportance={mockHandler} />);
+
+  const user = userEvent.setup();
+  const button = screen.getByText("Change importance");
+  await user.click(button);
+
+  expect(mockHandler.mock.calls).toHaveLength(1);
 });
