f22fbda0065f2f2d7e4686ed86d66b08ba3c639c
manzeelanepali <nepalimanzeela@gmail.com>
2022-12-12 17:09:29 +0545

    feat : added the beforeeach test so we should seperate the shared part into a before each block before running the each block test

--- form/cypress/e2e/note_app.cy.js
+++ form/cypress/e2e/note_app.cy.js
@@ -1,7 +1,20 @@
 describe("Note app", function () {
+    describe('Note app', function() {
+        beforeEach(function() {
+          cy.visit('http://localhost:3001')
+        })
+
+
+
+
   it("front page can be opened", function () {
     cy.visit("http://localhost:3001");
     cy.contains("Notes");
     cy.contains("Note app, TEJ Center ,2022");
+  });
+
+  it("login form can be opened", function () {
+    cy.visit("http://localhost:3001");
+    cy.contains("login").click();
   });
 });
