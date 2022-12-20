925c380343e1c8a157e04a7556531f72a265620b
manzeelanepali <nepalimanzeela@gmail.com>
2022-12-20 10:56:45 +0545

    feat : made to the state where new notes can be added

---
+++ part4/redux-notes/src/index.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {createStore } from "redux";
+
+const noteReducer = (state = [], action) => {
+  if (action.type === 'NEW_NOTE') {
+    state.push(action.data)
+    return state
+  }
+  return state
+}
+const store = createStore(noteReducer)
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1
+  }
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2
+  }
+})
+
+
+const generateId = () =>
+  Number((Math.random() * 1000000).toFixed(0))
+
+const App = () => {
+  const addNote = (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    store.dispatch({
+      type: 'NEW_NOTE',
+      data: {
+        content,
+        important: false,
+        id: generateId()
+      }
+    })
+  }
+    
+  
+  
+  
+
+  return(
+    <div>
+         <form onSubmit={addNote}>
+        <input name="note" /> 
+        <button type="submit">add</button>
+      </form>
+      <ul>
+        {store.getState().map(note=>
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        )}
+        </ul>
+    </div>
+  )
+}
+
+const renderApp = () => {
+  ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
+}
+renderApp()
+
+
+store.subscribe(renderApp)
