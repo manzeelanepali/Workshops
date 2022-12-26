936e8be3cf322699590f76195b8e14b20494800c
manzeelanepali <nepalimanzeela@gmail.com>
2022-12-26 10:01:28 +0545

    feat : refactoring code

--- part4/redux-notes/src/App.js
+++ part4/redux-notes/src/App.js
@@ -1,53 +1,16 @@
-import React from "react"
-import { toggleImportanceOf,createNote} from "./reducers/noteReducer"
-import { useSelector,useDispatch } from "react-redux"
+import React  from "react";
+import NewNote from "./components/NewNote";
+import Notes from "./components/Notes"
+// import { useSelector,useDispatch } from "react-redux"
 
 const App = () => {
-    const dispatch= useDispatch();
-    const notes = useSelector((state)=>state);
-
- 
-
-
-    const addNote = (event) => {
-      event.preventDefault()
-      const content = event.target.note.value
-      event.target.note.value = ''
-      dispatch(
-        createNote(content)
-        
-      )
-    }
-  
-   
-  
-    
-    const toggleImportance = (id) => {
-      
-      dispatch(toggleImportanceOf(id))
-      };
-    
-      
-    
-    
-    
-  
-    return(
+ return(
       <div>
-           <form onSubmit={addNote}>
-          <input name="note" /> 
-          <button type="submit">add</button>
-        </form>
-        <ul>
-          {notes.map(note=>
-            <li key={note.id}
-              onClick={() => toggleImportance(note.id)}>
-             {note.content} <strong>{note.important ? 'important' : ''}</strong>
-            </li>
-          )}
-          </ul>
-      </div>
-    )
-  }
+        <Notes/>
+          <NewNote/>
+          
+    </div>
+ )
+}
 
   export default App;
---
+++ part4/redux-notes/src/components/Notes.js
@@ -0,0 +1,35 @@
+
+import { toggleImportanceOf } from "../reducers/noteReducer";
+import { useSelector,useDispatch } from "react-redux";
+
+const Notes =(props)=>{
+    const dispatch= useDispatch();
+
+
+
+const notes = useSelector((state)=> state);
+
+
+
+const toggleImportance = (id) => {
+      
+    dispatch(toggleImportanceOf(id));
+    };
+
+
+
+
+
+ return(
+    <ul>
+    {notes.map((note)=>(
+        <li key={note.id}
+          onClick={() => toggleImportance(note.id)}>
+         {note.content} <strong>{note.important ? 'important' : ''}</strong>
+        </li>
+     ) )}
+      </ul>
+  )
+}
+    
+    export default Notes;
