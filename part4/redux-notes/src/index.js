0b5f3e9c88699f5d48555e3d003ce1670bb4f532
manzeelanepali <nepalimanzeela@gmail.com>
2022-12-27 15:16:10 +0545

    feat : moving the store to its own file store.js

--- part4/redux-notes/src/index.js
+++ part4/redux-notes/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import {createStore,combineReducers } from "redux";
-import { configureStore } from '@reduxjs/toolkit'
+// import { configureStore } from '@reduxjs/toolkit'
 // import noteReducer,{appendNote} from './reducers/noteReducer';} from './reducers/noteReducer';
-
-import noteReducer from './reducers/noteReducer';
-import filterReducer from './reducers/filterReducer';
+// import store from "./store"
+// import noteReducer from './reducers/noteReducer';
+// import filterReducer from './reducers/filterReducer';
 import { Provider } from "react-redux";
 import App from './App';
+import store from './store';
 // import { setNotes } from './reducers/noteReducer';
 
 
@@ -18,12 +19,7 @@ import App from './App';
 
 
 
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer
-  }
-})
+
 
 
   ReactDOM.createRoot(document.getElementById('root')).render(
