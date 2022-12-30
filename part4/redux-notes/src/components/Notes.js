f4f4d16e4981769fbdc49770c194cf105b6a55bd
manzeelanepali <nepalimanzeela@gmail.com>
2022-12-30 12:33:36 +0545

    feat : use of the mapDispatchProps and getting rid of the use dispatch

--- part4/redux-notes/src/components/Notes.js
+++ part4/redux-notes/src/components/Notes.js
@@ -1,26 +1,15 @@
 import { connect } from 'react-redux'
 import { toggleImportanceOf } from "../reducers/noteReducer";
-// import { useSelector,useDispatch } from "react-redux";
-import { useDispatch } from "react-redux";
-
+// import { useDispatch } from "react-redux";
 
 
 
 const Notes =(props)=>{
-const dispatch= useDispatch();
+  // const dispatch= useDispatch();
 
 
 
-    // const notesToShow = () => {
-    //   if ( props.filter === 'ALL' ) {
-    //     return props.notes
-    //   }
-      
-    //   return props.filter  === 'IMPORTANT'
-    //     ? props.notes.filter(note => note.important)
-    //     : props.notes.filter(note => !note.important)
-    // }
-
+  
 
 
 
@@ -31,36 +20,44 @@ return (
     {props.notes.map((note) => (
           <li
             key={note.id}
-            onClick={() => dispatch(toggleImportanceOf(note.id))}
+            onClick={() => props.toggleImportanceOf(note.id)}
           >
             {note.content} <strong>{note.important ? "important" : ""}</strong>
           </li>
         ))}
       </ul>
-    </div>
-  );
-};
+      </div>
+)
+    }
+
 
 
 
 const mapStateToProps = (state) => {
-  if ( state.filter === 'ALL' ) {
+   if ( state.filter === 'ALL' ) {
     return {
       notes: state.notes
-    }
-  }
-
-  return {
-    notes: (state.filter  === 'IMPORTANT' 
-      ? state.notes.filter(note => note.important)
-      : state.notes.filter(note => !note.important)
-    )
   }
 }
-  
+
+
+return {
+  notes: (state.filter  === 'IMPORTANT' 
+    ? state.notes.filter(note => note.important)
+    : state.notes.filter(note => !note.important)
+  )
+}
+}
+const mapDispatchToProps = {
+  toggleImportanceOf,
+}
 
 
 
 
-const ConnectedNotes = connect(mapStateToProps)(Notes)
+
+
+
+
+ const ConnectedNotes = connect(mapStateToProps,mapDispatchToProps)(Notes)
     export default ConnectedNotes;
