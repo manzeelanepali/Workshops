9624faa38f105155ad9fe02eac3b133ec9e8baf1
manzeelanepali <nepalimanzeela@gmail.com>
2022-12-30 13:56:29 +0545

    feat : refactoring adding notes component and removal of use dispatch and use selector methods

--- part4/redux-notes/src/components/Notes.js
+++ part4/redux-notes/src/components/Notes.js
@@ -1,63 +1,40 @@
-import { connect } from 'react-redux'
+import { connect } from "react-redux";
 import { toggleImportanceOf } from "../reducers/noteReducer";
 // import { useDispatch } from "react-redux";
 
-
-
-const Notes =(props)=>{
+const Notes = (props) => {
   // const dispatch= useDispatch();
 
-
-
-  
-
-
-
-
-return (
-  <div>
-    <ul>
-    {props.notes.map((note) => (
-          <li
-            key={note.id}
-            onClick={() => props.toggleImportanceOf(note.id)}
-          >
+  return (
+    <div>
+      <ul>
+        {props.notes.map((note) => (
+          <li key={note.id} onClick={() => props.toggleImportanceOf(note.id)}>
             {note.content} <strong>{note.important ? "important" : ""}</strong>
           </li>
         ))}
       </ul>
-      </div>
-)
-    }
-
-
-
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
-   if ( state.filter === 'ALL' ) {
+  if (state.filter === "ALL") {
     return {
-      notes: state.notes
+      notes: state.notes,
+    };
   }
-}
 
-
-return {
-  notes: (state.filter  === 'IMPORTANT' 
-    ? state.notes.filter(note => note.important)
-    : state.notes.filter(note => !note.important)
-  )
-}
-}
+  return {
+    notes:
+      state.filter === "IMPORTANT"
+        ? state.notes.filter((note) => note.important)
+        : state.notes.filter((note) => !note.important),
+  };
+};
 const mapDispatchToProps = {
   toggleImportanceOf,
-}
+};
 
-
-
-
-
-
-
-
- const ConnectedNotes = connect(mapStateToProps,mapDispatchToProps)(Notes)
-    export default ConnectedNotes;
+const ConnectedNotes = connect(mapStateToProps, mapDispatchToProps)(Notes);
+export default ConnectedNotes;
