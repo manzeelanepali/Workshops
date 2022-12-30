595b2da37d8a39393aa26ad46ee6a18636da825b
manzeelanepali <nepalimanzeela@gmail.com>
2022-12-30 13:40:47 +0545

    feat : Referencing action creators passed as props

--- part4/redux-notes/src/components/NewNote.js
+++ part4/redux-notes/src/components/NewNote.js
@@ -1,38 +1,31 @@
-
+import { connect } from "react-redux";
 import { createNote } from "../reducers/noteReducer";
-import { useDispatch } from "react-redux";
+// import { useDispatch } from "react-redux";
 // import noteService from '../services/notes'
 
-const NewNote=(props)=>{
-    const dispatch= useDispatch();
-    
+const NewNote = (props) => {
+  // const dispatch = useDispatch();
 
-    const addNote = (event) => {
-        event.preventDefault();
-       
-        const content = event.target.note.value
-        event.target.note.value = ''
-        dispatch(
-            createNote(content))
+  const addNote = (event) => {
+    event.preventDefault();
+    const content = event.target.note.value;
+    event.target.note.value = "";
+    props.createNote(content);
+  };
+  // const addNote = async (event) => {
+  //   event.preventDefault()
+  //   const content = event.target.note.value
+  //   event.target.note.value = ''
+  //   const newNote = await noteService.createNew(content)
+  //   dispatch(createNote(newNote))
+  // }
+
+  return (
+    <form onSubmit={addNote}>
+      <input name="note" />
+      <button type="submit">add</button>
+    </form>
+  );
 };
 
-
-// const addNote = async (event) => {
-//   event.preventDefault()
-//   const content = event.target.note.value
-//   event.target.note.value = ''
-//   const newNote = await noteService.createNew(content)
-//   dispatch(createNote(newNote))
-// }
-
-return(
-    
-    <form onSubmit={addNote}>
-    <input name="note" /> 
-    <button type="submit">add</button>
-  </form>
-  
-)
-}
-
-export default NewNote;
+export default connect(null, { createNote })(NewNote);
